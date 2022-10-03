5f44b729bb5efb9706a78eca982be4c062412fa7
Abdulrahman Almajdalwi <at.majdalawi@gmail.com>
2022-10-03 22:43:50 +0300

    Add updatetable function to the js file

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 // book object constructor details 
 
 let library_data = [];
+let i = 0;
 
 function Book (title, author, pages, dateofpub, readstatus) {
+    this.sr  = i;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -56,21 +58,60 @@ overlay.addEventListener('click', () => {
 })
 
 // get the values from the form 
+// and add values to the table
 
 let entrybutton = document.querySelector('.addentry');
+let tbody = document.querySelector('.table-body');
+let bookForm = document.querySelector('#book-form');
 
-entrybutton.addEventListener('click', () => {
-    let title = document.querySelector('#title').value;
-    let pages = document.querySelector('#pages').value;
-    let dop = document.querySelector('#dop').value;
-    let author = document.querySelector('#Author').value;
-    let readstatus = document.querySelector('#readstatus').checked;
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    let newBook = new Book(title,author,pages,dop,readstatus);
-    console.log(addToLibrary(newBook));
+    let title = document.querySelector('#title');
+    let pages = document.querySelector('#pages');
+    let dop = document.querySelector('#dop');
+    let author = document.querySelector('#Author');
+    let readstatus = document.querySelector('#readstatus');
+    console.log(readstatus.checked)
 
-    const popup = document.querySelector('#popupForm');
-    close_popup(popup);
+    let newBook = new Book(title.value,author.value,pages.value,dop.value,readstatus.checked);
+    i = i + 1;
+    addToLibrary(newBook);
+    updateTable(newBook);
+
+    // let bookForm = document.querySelector('#book-form');
+    bookForm.reset();
+
+
+    // const popup = document.querySelector('#popupForm');
+    // close_popup(popup);
 })
+
+function updateTable(book) {
+    let tbody = document.querySelector('.table-body');
+    let row = tbody.insertRow();
+
+    if (tbody.rows[0].cells.length == 1) {
+        let table = document.querySelector('#table');
+        
+        table.deleteRow(1);
+    }
+
+    for (const prop in book) {
+        if (book.hasOwnProperty(prop)) {
+            if (prop == 'sr') {
+                continue;
+            }
+            let cell = row.insertCell();
+            cell.textContent = book[prop];
+            console.log(book[prop])
+        }
+    }
+
+    row.setAttribute(`data-${book.sr}`,`rem-${book.sr}`)
+    let cell = row.insertCell();
+    cell.innerHTML = `<button id="rem-${book.sr}">&times</button>`
+}
+
 
 
